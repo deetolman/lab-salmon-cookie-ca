7e54246446b028caad0160b7410f8fc606966360
Dyanna Tolman <dyannast@gmail.com>
2018-08-02 18:06:52 -0700

    added some lines in stores-table.js

--- js/app.js
+++ js/app.js
@@ -2,29 +2,33 @@
 
 (function(module) {
     let html = module.html;
-    let StoresTable = module.s
+    let StoresTable = module.StoreTable;
+
     
 
 
     let template = function() {
         return html`
             <header>
-                <h1>Stores</h1>
+                <h1>Salmon Cookie Stores</h1>
             </header>
         
             <main></main>
          `;
 
     };
-    let someBs = html`<h1>Howdy</h1>`;
+
     class App { 
         render() {
             let dom = template();
 
             let main = dom.querySelector('main');
 
+            let storesTable = new StoresTable({
+
+            });
+
             
-            main.appendChild(someBs);
             return dom;
         }
     }
---
+++ js/stores-table.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function(module) {
+    let html = module.html;
+
+    let template = function() {
+        return html`
+            <section>
+                
+    }
+
+
+}
